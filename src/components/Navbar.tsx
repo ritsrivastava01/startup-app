65ba0288ad98ab57752a2b3705a375202a37471b
Ritesh <ritsrivastava@gmail.com>
2025-01-07 14:36:48 +0100

    feat: setup local font and customize the tailwind for project

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ import { auth, signIn, signOut } from '../../auth';
 export const NavBar = async () => {
   const session = await auth();
   return (
-    <header className='px-5 py-3 bg-white shadow-sm font-wonk-sans'>
+    <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
       <nav className='flex justify-between items-center'>
         <Link href='/'>
           <Image src='/logo.png' alt='Logo' width={144} height={30} />
@@ -17,25 +17,35 @@ export const NavBar = async () => {
               <Link href={'/startup/create'}>
                 <span className='mr-5'>Create</span>
               </Link>
-              <button
-                onClick={async () => {
+              <form
+                action={async () => {
                   'use server';
                   await signOut({ redirectTo: '/' });
                 }}>
-                Sign Out
-              </button>
+                <button type='submit'>Sign Out</button>
+              </form>
+
+              <form
+                action={async () => {
+                  'use server';
+                  await signOut({ redirectTo: '/' });
+                }}>
+                <button type='submit'>Logout</button>
+              </form>
+
               <Link href={`/user/${session?.user?.id}`}>
                 <span>{session.user.name}</span>
               </Link>
             </>
           ) : (
-            <button
-              onClick={async () => {
+            <form
+              action={async () => {
                 'use server';
-                await signIn();
+
+                await signIn('github');
               }}>
-              Sign In
-            </button>
+              <button type='submit'>Login</button>
+            </form>
           )}
         </div>
       </nav>
